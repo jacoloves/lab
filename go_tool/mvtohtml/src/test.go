eb0e452d467aeed1ab0cfaed2d1b60c29013d945
shotaro tanaka <5511068t@gmail.com>
2021-04-03 06:55:42 +0900

    mvtohtml testmodulenupdate

--- go_tool/mvtohtml/src/test.go
+++ go_tool/mvtohtml/src/test.go
@@ -2,8 +2,16 @@ package main
 
 import "fmt"
 
+func string_format(text string) string {
+	return fmt.Sprintf("<title>%s</title>\n", text)
+}
+
 func main() {
-	data := "<html>"
+	title_name := "yyyy_mm_dd.md"
+	data := "<!DOCTYPE html>\n"
+	data += "<html>\n"
+	data += string_format(title_name)
+	data += "<meta http-equiv=\"Content-type\" content=\"text/html;charset=UTF-8\">\n"
 
 	fmt.Println(data)
 }
