93a46821c68434917373c2fb1964ccdccd55af91
shotaro tanaka <5511068t@gmail.com>
2021-05-01 23:31:09 +0900

    refactor keybord input

--- tiro/tiro.c
+++ tiro/tiro.c
@@ -6,6 +6,9 @@
 #include <termios.h>
 #include <unistd.h>
 
+/*** defines ***/
+#define CTRL_KEY(k)((k) & 0x1f)
+
 /*** data ***/
 struct termios orig_termios;
 
@@ -35,20 +38,32 @@ void enableRawMode() {
   if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 
+char editorReadKey() {
+  int nread;
+  char c;
+  while((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+    if (nread == -1 && errno != EAGAIN) die("read");
+  }
+  return c;
+}
+
+/*** input ***/
+void editorProcessKeypress() {
+  char c = editorReadKey();
+
+  switch(c) {
+    case CTRL_KEY('q');
+      exit(0);
+      break;
+  }
+}
+
 /*** init ***/
 int main() {
   enableRawMode();
 
   while (1){
-    char c = '\0';
-    read(STDIN_FILENO, &c, 1);
-    if (read(STDIN_FILENO,  &c, 1) == -1 && errno != EAGAIN) die("read");
-    if (iscntrl(c)) {
-      printf("%d\r\n", c);
-    } else {
-      printf("%d ('%c')\r\n", c, c);
-    }
-    if (c == 'q') break;
+    editorProcessKeypress();
   }
 
   return 0;
