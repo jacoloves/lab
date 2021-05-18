1db759352c8eb3c3c999cf4fabbecd6fa77f58f7
s.tanaka <s.tanaka@core-tech.jp>
2021-05-18 15:55:37 +0900

    20210518

--- tiro/tiro.c
+++ tiro/tiro.c
@@ -112,11 +112,12 @@ void abFree(struct abuf *ab) {
 }
 
 /*** output ***/
-void editorDrawRows(struct abuf *ab) {
+void editorDrawRows() {
   int y;
   for (y = 0; y < E.screenrows; y++) {
     abAppend(ab, "~", 1);
 
+    abAppend(ab, "\x1b[K", 3);
     if (y < E.screenrows - 1) {
       abAppend(ab, "\r\n", 2);
     }
@@ -126,12 +127,13 @@ void editorDrawRows(struct abuf *ab) {
 void editorRefreshScreen() {
   struct abuf ab = ABUF_INIT;
 
-  abAppend(&ab, "\x1b[2J", 4);
+  abAppend(&ab, "\x1b[?25l", 6);
   abAppend(&ab, "\x1b[H", 3);
 
   editorDrawRows(&ab);
 
   abAppend(&ab, "\x1b[H", 3);
+  abAppend(&ab, "\x1b[?25h", 6);
 
   write(STDOUT_FILENO, ab.b, ab.len);
   abFree(&ab);
