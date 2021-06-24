1eb5b1d524c05b86afb440ac7ab381c3117956ca
jacoloves <5511068t@gmail.com>
2021-06-25 08:18:44 +0900

    color add

--- go_tool/gojork/main.go
+++ go_tool/gojork/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"math/rand"
 	"os"
 	"time"
 
@@ -21,6 +22,28 @@ func clear() {
 	fmt.Fprintln(out, "\x1b[1;1H")
 }
 
+func color() {
+	rand.Seed(time.Now().UnixNano())
+	rand_num := rand.Intn(7)
+
+	switch rand_num {
+	case 0:
+		fmt.Print("\x1b[31m")
+	case 1:
+		fmt.Print("\x1b[32m")
+	case 2:
+		fmt.Print("\x1b[33m")
+	case 3:
+		fmt.Print("\x1b[34m")
+	case 4:
+		fmt.Print("\x1b[35m")
+	case 5:
+		fmt.Print("\x1b[36m")
+	case 6:
+		fmt.Print("\x1b[37m")
+	}
+}
+
 func main() {
 	file, err := packr.NewBox("./text").Open("gologo.txt")
 	if err != nil {
@@ -36,6 +59,7 @@ func main() {
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		if scanner.Text() == endline {
+			color()
 			fmt.Println(buffer)
 			time.Sleep(5 * time.Second)
 
