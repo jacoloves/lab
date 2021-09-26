ba05cc6019222427e954504a9f9b8dcdfc5711ed
jacoloves <5511068t@gmail.com>
2021-09-27 01:00:22 +0900

    day 6

---
+++ rust_project/adder/src/lib.rs
@@ -0,0 +1,55 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(larger.can_hold(&smaller));
+    }
+
+    #[test]
+    fn smaller_cannot_hold_larger() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(!smaller.can_hold(&larger));
+    }
+
+    #[test]
+    fn it_adds_two() {
+        assert_eq!(4, add_two(2));
+    }
+}
+
+fn main() {}
--- rust_project/lifetime_smp/src/main.rs
+++ rust_project/lifetime_smp/src/main.rs
@@ -1,4 +1,92 @@
-fn longest(x: &str, y: &str) -> &str {
+fn smp_main5() {
+    use std::fmt::Display;
+
+    fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
+    where
+        T: Display,
+    {
+        println!("Announcement! {}", ann);
+        if x.len() > y.len() {
+            x
+        } else {
+            y
+        }
+    }
+}
+
+fn smp_main4() {
+    struct ImportantExcerpt2<'a> {
+        part: &'a str,
+    }
+
+    impl<'a> ImportantExcerpt2<'a> {
+        fn announce_and_return_part(&self, announcement: &str) -> &str {
+            println!("Attention please: {}", announcement);
+            self.part
+        }
+    }
+
+    impl<'a> ImportantExcerpt2<'a> {
+        fn level(&self) -> i32 {
+            3
+        }
+    }
+}
+
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
+
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
+fn smp_main3() {
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
+}
+
+fn longest3<'a>(x: &str, y: &str) -> &'a str {
+    let result = String::from("really long string");
+    result.as_str();
+}
+
+fn longest2<'a>(x: &'a str, y: &str) -> &'a str {
+    x
+}
+
+fn smp_main2() {
+    let string1 = String::from("long string is long");
+    let result;
+    {
+        let string2 = String::from("xyz");
+        result = longest(string1.as_str(), string2.as_str());
+    }
+    println!("The longest string is {}", result);
+}
+
+fn smp_main() {
+    let string1 = String::from("long string is long");
+
+    {
+        let string2 = String::from("xyz");
+        let result = longest(string1.as_str(), string2.as_str());
+        println!("The longest string is {}", result);
+    }
+}
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     if x.len() > y.len() {
         x
     } else {
@@ -13,4 +101,6 @@ fn main() {
     let result = longest(string1.as_str(), string2);
 
     println!("The longest string is {}", result);
+
+    smp_main();
 }
