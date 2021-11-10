8c1d514cf5ac38f002e9063e91e44da5fb6f108d
jacoloves <5511068t@gmail.com>
2021-11-10 23:52:01 +0900

    day1

--- go_tool/qf/main.go
+++ go_tool/qf/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+//first_indent_clauses := [..] string{"SELECT", "FROM", "WHERE"}
 
 func main() {
-	fmt.Println("vim-go")
+	filename := os.Args[1]
+	fp, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer fp.Close()
+
+	scanner := bufio.NewScanner(fp)
+	for scanner.Scan() {
+		line := scanner.Text()
+		fmt.Printf(line)
+	}
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
 }
