ac630fab4745789170a4e931fb9a5135ef45ee89
jacoloves <5511068t@gmail.com>
2021-11-12 23:51:31 +0900

    day 2

--- go_tool/qf/main.go
+++ go_tool/qf/main.go
@@ -22,22 +22,56 @@ const (
 	EQUAL              = "="
 )
 
+const (
+	SELECT_MODE = 100
+	OTHER_MODE  = iota
+)
+
+func select_word_fornatted(word string, query *string) {
+		if word == "SELECT" {
+				*query += word + "\n" 
+		} else if word == "," {
+				*query += "\t" + word + " "
+		} else if  {
+				*query += word
+		}
+}
+
 func query_formatted(queryes []string) {
 	first_indent_clauses := [...]string{"SELECT", "FROM", "WHERE"}
 	var query_depth_level int
 	var no_caluses string
+	var current_mode int
+	var genetare_query string
 	operator_flg := false
 	queryes[len(queryes)-1] = strings.Replace(queryes[len(queryes)-1], ";", "", 1)
 	for _, s := range queryes {
 		query_depth_level = NO_CALUSE
 		for _, v := range first_indent_clauses {
-			if v == strings.ToUpper(s) {
+			switch strings.ToUpper(v) {
+			case "SELECT":
+				query_depth_level = DEPTH_LEVEL_ONE
+				current_mode = SELECT_MODE
+				break
+			case "FROM":
+			case "WHERE":
+				current_mode = OTHER_MODE
 				query_depth_level = DEPTH_LEVEL_ONE
 				break
 			}
+			if query_depth_level == DEPTH_LEVEL_ONE {
+				break
+			}
 		}
-		if query_depth_level == NO_CALUSE {
-			if s == GREATER_THAN || s == GREATER_THAN_EQUAL || s == LESS_THAN || s == LESS_THAN_EQUAL || s == EQUAL {
+		if current_mode == SELECT_MODE {
+				select_word_fornatted(s, &genetare_query)	
+		} else if query_depth_level == NO_CALUSE {
+			switch s {
+			case GREATER_THAN:
+			case GREATER_THAN_EQUAL:
+			case LESS_THAN:
+			case LESS_THAN_EQUAL:
+			case EQUAL:
 				operator_flg = true
 			}
 			if operator_flg {
