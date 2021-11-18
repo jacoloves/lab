31acebe82ccd7f8a8305ded6b38472c195dfba3a
jacoloves <5511068t@gmail.com>
2021-11-19 01:58:06 +0900

    wsudo create done

--- go_tool/win_sudo/main.go
+++ go_tool/win_sudo/main.go
@@ -1,24 +1,56 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"log"
 	"os"
+	"os/exec"
 )
 
-func arg_check() string {
+func arg_check() (string, string) {
 	if len(os.Args) < 2 {
 		s := "Usage: sudo.exe [commad1] [commad2]"
 		fmt.Println(s)
 		os.Exit(1)
 	}
 
-	if len(os.Args) = 2 {
-		
+	// arg is 2
+	if len(os.Args) == 3 {
+		return os.Args[1], os.Args[2]
 	}
-		
+
+	if len(os.Args) > 4 {
+		s := "Usage: sudo.exe [commad1] [commad2]"
+		fmt.Println(s)
+		os.Exit(1)
+	}
+
+	// arg is 1
+	return os.Args[1], ""
+}
+
+func execute(com1 string, com2 string) {
+	exec_command := ""
+	if com2 == "" {
+		exec_command = fmt.Sprintf("powershell -command start-process %s -verb runas", com1)
+	} else {
+		exec_command = fmt.Sprintf("powershell -command start-process %s -verb runas %s", com1, com2)
+	}
+
+	cmd := exec.Command("powershell.exe", exec_command)
+	var stderr bytes.Buffer
+	cmd.Stderr = &stderr
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func main() {
 	// arg check
-	commad1,commad2 := arg_check()
+	command1, command2 := arg_check()
+
+	//execute
+	execute(command1, command2)
 }
