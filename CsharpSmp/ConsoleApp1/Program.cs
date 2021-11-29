de650c180cd1fb4d364ca19d892e98e88eadbeb9
jacoloves <5511068t@gmail.com>
2021-11-30 07:50:05 +0900

    page 40

---
+++ CsharpSmp/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static int Echo(int x)
+    {
+        Console.WriteLine(x);
+        return x;
+    }
+
+    static void Main()
+    {
+        var s = Echo(2) + Echo(3) * Echo(4);
+    }
+}
