c6e5bae1f871c43fb8577a104b6f84cd25c3bf1a
jacoloves <5511068t@gmail.com>
2021-12-01 23:49:46 +0900

    page106

---
+++ CsharpSmp/sample7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class CheckedSample
+{
+    static void Main()
+    {
+        try
+        {
+            checked
+            {
+                sbyte a = 64;
+                sbyte b = 65;
+
+                sbyte c = (sbyte)(a + b);
+            }
+        }
+        catch (OverflowException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
