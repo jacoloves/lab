9144c097aaa317a56a0ffe705ea929567253de9d
jacoloves <5511068t@gmail.com>
2021-12-02 18:52:23 +0900

    mod test_arch

--- go_tool/win_sudo/test_arch.go
+++ go_tool/win_sudo/test_arch.go
@@ -12,20 +12,40 @@ const DQUOTAION_MODE = 2
 func main() {
 	mode := 0
 	commond_cnt := 0
-	if strings.Contains(os.Args[1], "C:\\") {
+	program_cmd := ""
+	loop_cnt := 0
+	if strings.Contains(os.Args[1], "C:\\") || strings.Contains(os.Args[1], "D:\\") {
 		mode = ABSOLUTE_MODE
 	}
 
 	if mode == ABSOLUTE_MODE {
 		for _, v := range os.Args[1:] {
-			if strings.Contains(v, "C:\\") && commond_cnt > 0 {
+			if (strings.Contains(v, "C:\\") || strings.Contains(os.Args[1], "D:\\")) && commond_cnt > 0 {
 				break
 			}
 			commond_cnt++
 		}
 	}
 
-	for _, v := range os.Args[1 : commond_cnt+1] {
-		fmt.Println(v)
+	if commond_cnt == 1 {
+		mode = DQUOTAION_MODE
 	}
+
+	if mode == DQUOTAION_MODE {
+		program_cmd = "\"" + os.Args[1] + "\""
+	} else {
+		for _, v := range os.Args[1 : commond_cnt+1] {
+			if loop_cnt == 0 {
+				program_cmd += "\"" + v + " "
+			} else if loop_cnt == commond_cnt-1 {
+				program_cmd += v + "\""
+			} else {
+				program_cmd += v + " "
+			}
+
+			loop_cnt++
+		}
+	}
+
+	fmt.Println(program_cmd)
 }
