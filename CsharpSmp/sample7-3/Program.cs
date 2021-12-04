b7be13cfd7ecaa00a7c0905b0212c0d1ca0c4dda
jacoloves <5511068t@gmail.com>
2021-12-04 17:00:59 +0900

    page 308

---
+++ CsharpSmp/sample7-3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Base { }
+class Derived1 : Base { }
+class Derived2 : Base { }
+
+class DowncastTest
+{
+    static void Main()
+    {
+        Base b;
+
+        b = new Derived1();
+
+        if (b is Derived1)
+            Console.WriteLine("b = new Derived1();");
+
+        b = new Derived2();
+
+        if (b is Derived1)
+            Console.WriteLine("b = new Derived2");
+    }
+}
