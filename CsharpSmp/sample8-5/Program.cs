5c470b0cd15985488eee73f398cc4794fc59c32a
jacoloves <5511068t@gmail.com>
2021-12-05 08:31:17 +0900

    page 369

---
+++ CsharpSmp/sample8-5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+
+class Program
+{
+    static void Main()
+    {
+        var x = new CollectionInitializable
+        {
+            "abc",
+            "def",
+            "ghi",
+        };
+    }
+}
+
+class CollectionInitializable : IEnumerable
+{
+    public void Add(string item)
+    {
+        Console.WriteLine($"{item} Added");
+    }
+
+    public IEnumerator GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+}
