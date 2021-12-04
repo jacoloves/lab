3a3b9a9c0b4a65e32451857cc25b80d47a3b1e8a
jacoloves <5511068t@gmail.com>
2021-12-04 20:55:01 +0900

    page 335

---
+++ CsharpSmp/sample7-9/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+interface IMessageCollection
+{
+    void Add(string value);
+    IEnumerable<string> Messages { get; }
+}
+
+interface INotificationCollection
+{
+    void Add(string value);
+    IEnumerable<string> Notifications { get; }
+}
+
+class ServiceManager : IMessageCollection, INotificationCollection
+{
+    private List<string> notifications = new List<string>();
+    private List<string> messages = new List<string>();
+    public IEnumerable<string> Notifications
+    {
+        get { return this.notifications; }
+    }
+    public IEnumerable<string> Messages
+    {
+        get { return this.messages; }
+    }
+    void INotificationCollection.Add(string value)
+    {
+        this.notifications.Add(value);
+    }
+
+    void IMessageCollection.Add(string value)
+    {
+        this.messages.Add(value);
+    }
+}
+
+class Sample
+{
+    static void Main()
+    {
+        var manager = new ServiceManager();
+        manager.Add("hello!");
+        IMessageCollection messageManager = manager;
+        INotificationCollection notificationManager = manager;
+        messageManager.Add("hello!");
+        notificationManager.Add("1 new message");
+    }
+}
