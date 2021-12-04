ec66224c7b2aba4e28a6d88ee43206f5c7878250
jacoloves <5511068t@gmail.com>
2021-12-04 17:40:23 +0900

    page 319

---
+++ CsharpSmp/sample7-7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Base
+{
+    public virtual void Test()
+    {
+        Console.WriteLine("Base.Test()");
+    }
+}
+
+class Derived : Base
+{
+    public override void Test()
+    {
+        Console.WriteLine("Derived Test()");
+    }
+}
+
+class VirtualTest
+{
+    static void Main()
+    {
+        Base a = new Base();
+        a.Test();
+
+        Base b = new Derived();
+        b.Test();
+
+        Derived c = new Derived();
+        c.Test();
+    }
+}
