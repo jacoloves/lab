b1734decae1d6a113b1da26635f29c25ff3f4291
jacoloves <5511068t@gmail.com>
2021-12-04 10:06:11 +0900

    Revert "Revert "page 275""
    This reverts commit e1b30adc4025c91ba3a339d36038d662810b8d69.

---
+++ CsharpSmp/sample6-9/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+class Complex
+{
+    private double re;
+    private double im;
+
+    public double Re
+    {
+        set { this.re = value; }
+        get { return this.re; }
+    }
+
+    public double Im
+    {
+        set { this.im = value; }
+        get { return this.im; }
+    }
+
+    public double Abs
+    {
+        get { return Math.Sqrt(re * re + im * im); }
+    }
+}
+
+class PropertySample
+{
+    static void Main()
+    {
+        Complex c = new Complex();
+        
+        c.Re = 4;
+        c.Im = 3;
+
+        Console.Write($"|{c.Re} + ");
+        Console.Write($"{c.Im}i| = ");
+        Console.WriteLine($"{c.Abs}");
+    }
+}
