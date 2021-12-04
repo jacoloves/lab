ca46fe4abe99f4b8d3ec9a513fd7413b134a19d5
jacoloves <5511068t@gmail.com>
2021-12-04 09:08:17 +0900

    page 268

---
+++ CsharpSmp/sample6-5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class Complex
+{
+    public double Abs;
+    public double Arg;
+
+    public void Set(double x, double y)
+    {
+        this.Abs = Math.Sqrt(x * x + y * y);
+        this.Arg = Math.Atan2(y, x);
+    }
+}
+
+class ConcealSample
+{
+    static void Main()
+    {
+        Complex c = new Complex();
+        c.Set(4, 3);
+
+        Console.WriteLine($"|c| = {c.Abs}");
+    }
+}
