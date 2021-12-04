a94a44100c04ded953710e3c790291fa914c8fb9
jacoloves <5511068t@gmail.com>
2021-12-04 16:52:30 +0900

    page 306

---
+++ CsharpSmp/sample7-2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Base { }
+class Dervied1 : Base { }
+class Dervied2 : Base { }
+
+class DowncastTest
+{
+    static void Main()
+    {
+        Dervied1 d1 = new Dervied1();
+        Dervied2 d2 = new Dervied2();
+
+        Base b;
+        Dervied1 d;
+
+        b = d1;
+        d = (Dervied1)b;
+
+        b = d2;
+        d = (Dervied2)b;
+    }
+}
