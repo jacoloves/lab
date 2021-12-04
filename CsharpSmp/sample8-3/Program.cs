931a87aa84aedcbec87b7183f498d2582c740c5a
jacoloves <5511068t@gmail.com>
2021-12-05 08:18:42 +0900

    page 365

---
+++ CsharpSmp/sample8-3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        var p = new Point(1, 2);
+        p += new Point(2, 3);
+        Console.WriteLine(p);
+    }
+}
+
+struct Point
+{
+    private int _x;
+    private int _y;
+    public int X { get { return _x; } }
+    public int Y { get { return _y; } }
+    public Point(int x, int y) { _x = x; _y = y; }
+
+    public static Point operator +(Point p, Point q)
+    {
+        return new Point(p.X + q.X, p.Y + q.Y);
+    }
+    public override string ToString()
+    {
+        return $"{this.X} {this.Y}";
+    }
+}
