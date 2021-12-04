46abbec54a8e86acd22349ca655bb8fca0b6e425
jacoloves <5511068t@gmail.com>
2021-12-04 09:03:54 +0900

    page 267

---
+++ CsharpSmp/sample6-4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Complex
+{
+    public double Re;
+    public double Im;
+
+    public double Abs()
+    {
+        return Math.Sqrt(Re * Re + Im * Im);
+    }
+}
+
+class ConcealSample
+{
+    static void Main()
+    {
+        Complex c = new Complex();
+        c.Re = 4;
+        c.Im = 3;
+
+        Console.WriteLine("|c| = {0}\n", c.Abs());
+    }
+}
