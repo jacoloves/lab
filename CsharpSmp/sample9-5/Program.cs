f7007e48c971d7d3bbcd2dce8a43bcb19d58dc65
jacoloves <5511068t@gmail.com>
2021-12-05 21:57:06 +0900

    page 398

---
+++ CsharpSmp/sample9-5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Sample
+{
+    private static int Square(int x)
+    {
+        return x * x;
+    }
+
+    static void Main()
+    {
+        var data = new[] { 1, 2, 3, 4, 5 };
+
+        var s1 = data.Select(Square);
+
+        var s2 = data.Select(delegate(int x) { return x; });
+
+        var s3 = data.Select(x => x * x);
+
+        Console.WriteLine(s1);
+        Console.WriteLine(s2);
+        Console.WriteLine(s3);
+    }
+}
