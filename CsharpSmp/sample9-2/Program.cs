7dee98e468b7e200c5915ac7a68c589795b6c6bc
jacoloves <5511068t@gmail.com>
2021-12-05 21:52:09 +0900

    page 387-396

---
+++ CsharpSmp/sample9-2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+delegate void SomeDelegate(int a);
+
+class DelegateTest
+{
+    static void Main()
+    {
+        SomeDelegate a = A;
+        a(256);
+    }
+
+    static void A(int n)
+    {
+        Console.WriteLine($"A({n})が呼ばれました");
+    }
+}
---
+++ CsharpSmp/sample9-4/Program.cs
@@ -0,0 +1,24 @@
+﻿class Smaple
+{
+    static void Filter(List<int> input, Predicate<int> pred)
+    {
+        foreach (var x in input)
+        {
+            if (pred(x))
+                Console.WriteLine(x);
+        }
+    }
+
+    /*static bool IsOver3(int x)
+    {
+        return x > 3;
+    }*/
+
+    static void Main()
+    {
+        var foo = new List<int>() { 1, 2, 3, 4, 5 };
+        /*Filter(foo, IsOver3);*/
+        Filter(foo, x => x > 3);
+        //foo.Where(x => x > 3);
+    }
+}
