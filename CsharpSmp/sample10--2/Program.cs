0c12e1926583d5a4d5115364606542100c0c359a
jacoloves <5511068t@gmail.com>
2021-12-06 09:30:31 +0900

    page 432

---
+++ CsharpSmp/sample10--2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+struct Point
+{
+    public int X, Y;
+    public Point(int x, int y) { this.X = x; this.Y = y; }
+    public override string ToString()
+        => $"({this.X}, {this.Y})";
+}
+
+class Sample
+{
+    static void Main()
+    {
+        Console.WriteLine("値型の場合");
+        Point a = new Point(2, 15);
+        Point b = a;
+        Point c = a;
+        Console.WriteLine($"a: {a}\nb: {b}\nc: {c}");
+        b.X = 0;
+        Console.WriteLine($"a: {a}\nb: {b}\nc: {c}");
+    }
+}
