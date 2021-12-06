4391349eda20ca30dd277150cdaaa4241a01695e
jacoloves <5511068t@gmail.com>
2021-12-06 21:47:50 +0900

    page 479

---
+++ CsharpSmp/sample10-10/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class OutTest
+{
+    static void Main()
+    {
+        int n;
+        string s;
+        do
+        {
+            Console.Write("Input Number: ");
+            s = Console.ReadLine();
+        } while (!ParseInt(s, out n));
+
+        Console.WriteLine($"Number is {n}");
+
+    }
+    static bool ParseInt(string str, out int value)
+    {
+        int val = 0;
+        foreach(char c in str)
+        {
+            if(c <'0'|| '9' < c)
+            {
+                value = default(int);
+                return false;
+            }
+
+            var i = c - '0';
+            val = val * 10 + i;
+        }
+        value = val;
+        return true;
+    }
+}
