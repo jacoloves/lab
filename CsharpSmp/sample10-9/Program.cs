0ba1d8e7b151d4796288f8f80dad90324a842986
jacoloves <5511068t@gmail.com>
2021-12-06 20:26:52 +0900

    page 463

---
+++ CsharpSmp/sample10-9/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Sample
+{
+    static void Main()
+    {
+        int a;
+        Test(out a);
+        Console.WriteLine("{0}\n", a);
+    }
+
+    static void Test(out int a)
+    {
+        a = 10;
+    }
+}
