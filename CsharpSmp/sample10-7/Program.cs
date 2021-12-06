04cf234d73ef9e05a227e85ee8ab5e09bc427af9
jacoloves <5511068t@gmail.com>
2021-12-06 20:12:46 +0900

    page 459

---
+++ CsharpSmp/sample10-7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Sample
+{
+    static ref int RefMax(ref int a, ref int b)
+    {
+        if(a >= b)
+        {
+            return ref a;
+        }
+        else
+        {
+            return ref b;
+        }
+    }
+
+    static void Main()
+    {
+        int p = 10, q = 20;
+
+        ref var a = ref RefMax(ref p, ref q);
+
+        var b = RefMax(ref p, ref q);
+        Console.WriteLine($"{a} {b}");
+
+        q = 0;
+        Console.WriteLine($"{a} {b}");
+    }
+}
