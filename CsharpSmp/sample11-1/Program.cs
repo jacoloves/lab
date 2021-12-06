6925c3706b35c0366bda99d775096fd8dbbba756
jacoloves <5511068t@gmail.com>
2021-12-06 22:06:44 +0900

    page 484

---
+++ CsharpSmp/sample11-1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+class IntStringPair
+{
+    public int Key { get; set; }
+    public string Value { get; set; }
+    public IntStringPair(int key, string value)
+    {
+        this.Key = key;
+        this.Value = value;
+    }
+}
+
+class IntIntPair
+{
+    public int Key { get; set; }
+    public int Value { get; set; }
+    public IntIntPair(int key, int value)
+    {
+        this.Key=key;
+        this.Value=value;
+    }
+}
+
+class KeyValuePair<TKey, TValue>
+{
+    public TKey Key { get; set; }
+    public TValue Value { get; set; }
+    public KeyValuePair(TKey key, TValue value)
+    {
+        this.Key = key;
+        this.Value = value;
+    }
+}
+
+class Sample
+{
+    static void Main()
+    {
+        /*var studentName = new IntStringPair(1, "test");
+        var studentAge = new IntIntPair(1, 28);*/
+
+        var studentNameT = new KeyValuePair<int, string>(1, "test2");
+        var studentValueT = new KeyValuePair<int, int>(1, 11);
+
+        Console.WriteLine(studentNameT.Value);
+        Console.WriteLine(studentValueT.Value);
+    }
+}
