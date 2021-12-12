489267e5b1a0e1c7ce91f246a3287c3ef84f7fba
jacoloves <5511068t@gmail.com>
2021-12-12 10:21:59 +0900

    page 245

---
+++ smpVCsharp/WhereNumber/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Sample
+{
+    static void Main()
+    {
+        int[] numbers = { 1, 2, 3, 4, 5 };
+        var query = from n
+                    in numbers
+                    where n >= 2 && n <= 4
+                    select n;
+        foreach (var a in query) Console.WriteLine(a);
+    }
+}
