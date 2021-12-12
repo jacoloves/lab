c072ebadfe0ec6a975ae5a0ce15eab57394ca788
jacoloves <5511068t@gmail.com>
2021-12-12 10:17:13 +0900

    page 242

---
+++ smpVCsharp/IteratorDays/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+
+class Sample
+{
+    static void Main()
+    {
+        foreach(DateTime dt in Test(10))
+        {
+            Console.WriteLine(dt.ToString("yyyy年MM月dd日（ddd）"));
+        }
+
+        static IEnumerable Test(int days)
+        {
+            DateTime dt = DateTime.Today;
+            for (int i =0; i < days;)
+            {
+                if ((dt.DayOfWeek != DayOfWeek.Saturday) &&
+                    (dt.DayOfWeek != DayOfWeek.Sunday))
+                {
+                    yield return dt;
+                    i++;
+                }
+                dt = dt.AddDays(1);
+            }
+        }
+    }
+}
