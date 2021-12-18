66d85345f5b0def688481507ce2047c30bc70361
jacoloves <5511068t@gmail.com>
2021-12-18 17:58:54 +0900

    reponder, emotion追加

---
+++ GirlFriend/GirlFriend/Cchan.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GirlFriend
+{
+    internal class Cchan
+    {
+        private string _name;
+        private Cdictionary _dictionary;
+        private CchanEmotion _emotion;
+        private RandomResponder _res_random;
+        private RepeatResponder _res_repeat;
+        private PatternResponder _res_pattern;
+        private Responder _responder;
+
+        public string Name
+        {
+            get => _name;
+        }
+
+        public CchanEmotion Emotion
+        {
+            get => _emotion;
+        }
+
+        public Cchan(string name)
+        {
+            _name = name;
+            _dictionary = new Cdictionary();
+            _emotion = new CchanEmotion(_dictionary);
+            _res_repeat = new RepeatResponder("Repeat", _dictionary);
+            _res_random = new RandomResponder("Random", _dictionary);
+            _res_pattern = new PatternResponder("Pattern", _dictionary);
+            _responder = new Responder("Responder", _dictionary);
+        }
+
+        public string Dialogue(string input)
+        {
+            _emotion.Update(input);
+            Random rnd = new();
+            int num = rnd.Next();
+
+            if (num < 6)
+                _responder = _res_pattern;
+            else if(num < 9)
+                _responder = _res_random;
+            else
+                _responder = _res_repeat;
+
+            return _responder.Response(
+                input,
+                _emotion.Mood);
+        }
+
+        public string GetName()
+        {
+            return _responder.Name;
+        }
+    }
+}
---
+++ GirlFriend/GirlFriend/PatternResponder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GirlFriend
+{
+    internal class PatternResponder : Responder
+    {
+        public PatternResponder(string name, Cdictionary dic) : base(name, dic)
+        { 
+        }
+
+        public override string Response(string input, int mood)
+        {
+            foreach (ParseItem parseItem in Cdictionary.Pattern)
+            {
+                string mtc = parseItem.Match(input);
+
+                if (String.IsNullOrEmpty(mtc) == false)
+                {
+                    string resp = parseItem.Choice(mood);
+
+                    if (resp != null)
+                        return resp.Replace("%match%", mtc);
+                }
+            }
+
+            int seed = Environment.TickCount;
+            Random rdm = new(seed);
+
+            return Cdictionary.Random[rdm.Next(0, Cdictionary.Random.Count)];
+        }
+    }
+}
