bbdbfc16d72cb0537e60c0a99bec48b2f24b9195
jacoloves <5511068t@gmail.com>
2022-01-05 23:07:22 +0900

    beep add

--- go_tool/gpomo/gpomo.go
+++ go_tool/gpomo/gpomo.go
@@ -3,10 +3,15 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"math/big"
 	"os"
 	"os/signal"
 	"time"
+
+	"github.com/faiface/beep"
+	"github.com/faiface/beep/mp3"
+	"github.com/faiface/beep/speaker"
 )
 
 func clear() {
@@ -57,6 +62,28 @@ func mod(val float64, mod int64) int64 {
 	return raw.Mod(raw, big.NewInt(mod)).Int64()
 }
 
+func outputMusuic() {
+	f, err := os.Open("./clock.mp3")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	streamer, format, err := mp3.Decode(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer streamer.Close()
+
+	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+
+	done := make(chan bool)
+	speaker.Play(beep.Seq(streamer, beep.Callback(func() {
+		done <- true
+	})))
+
+	<-done
+}
+
 func main() {
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
@@ -71,10 +98,12 @@ func main() {
 		clear()
 		progressTime()
 		fmt.Print("Please Push Enter")
+		outputMusuic()
 		bufio.NewReader(os.Stdin).ReadBytes('\n')
 		clear()
 		breakTime()
 		fmt.Print("Please Push Enter")
+		outputMusuic()
 		bufio.NewReader(os.Stdin).ReadBytes('\n')
 	}
 }
