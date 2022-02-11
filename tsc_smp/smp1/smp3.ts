faeb0473c5023175f55becc2b6b73e68d450257c
jacoloves <5511068t@gmail.com>
2022-02-12 00:17:07 +0900

    tsc smp page29

---
+++ tsc_smp/smp1/smp3.ts
@@ -0,0 +1,5 @@
+function toInt(value: number) {
+    return value.toFixed(0);
+}
+
+console.log(toInt(<any>'hoge'));
---
+++ tsc_smp/smp1/smp4.ts
@@ -0,0 +1,6 @@
+const DATA = [1, 2, 3];
+DATA[0] = 10;
+console.log(DATA);
+
+let data2: string[] = ['Java', 'Python', 'PHP', 'Ruby', 'C#'];
+console.log(data2[0]);
