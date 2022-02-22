4765fda00607e284f69a6814b8f724d7174a5b89
jacoloves <5511068t@gmail.com>
2022-02-22 14:21:06 +0900

    220222

--- rust_project/msRsut_smp/chap8-smp/src/main.rs
+++ rust_project/msRsut_smp/chap8-smp/src/main.rs
@@ -96,6 +96,79 @@ impl AsJson for Cat {
     }
 }
 
+#[derive(Debug)]
+struct Counter {
+    length: usize,
+    count: usize,
+}
+
+impl Counter {
+    fn new(length: usize) -> Counter {
+        Counter { 
+            count: 0,
+            length,
+        }
+    }
+}
+
+impl Iterator for Counter { 
+    type Item = usize;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.count += 1;
+        if self.count <= self.length {
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
+
+struct Groups<T> {
+    inner: Vec<T>,
+}
+
+impl<T> Groups<T> {
+    fn new(inner: Vec<T>) -> Self {
+        Groups { inner }
+    }
+}
+
+impl<T: PartialEq> Iterator for Groups<T> {
+    type Item = Vec<T>;
+
+    //TODO: this
+    fn next(&mut self) -> Option<Self::Item> {
+
+        if self.inner.is_empty() {
+            return None;
+        }
+        
+        let mut cursor = 1;
+        let first = &self.inner[0];
+        for element in &self.inner[1..] {
+            if element == first {
+                cursor += 1; 
+            } else {
+                break;
+            }
+        }
+
+        let items = self.inner.drain(0..cursor).collect();
+        Some(items)
+     }
+}
+
+struct Container<T> {
+    value: T,
+}
+
+impl<T> Container<T> {
+    pub fn new(value: T) -> Self {
+        Container { value }
+    }
+}
+
 fn main() {
     let boolean = Point { x: true, y: false };
     let integer = Point { x: 1, y: 9 };
@@ -148,4 +221,64 @@ fn main() {
     };
 
     send_data_as_json(&kitty);
+
+    println!("--- Unit 6/10 ---");
+    let mut counter = Counter::new(6);
+    println!("Counter just created: {:#?}", counter);
+
+    assert_eq!(counter.next(), Some(1));
+    assert_eq!(counter.next(), Some(2));
+    assert_eq!(counter.next(), Some(3));
+    assert_eq!(counter.next(), Some(4));
+    assert_eq!(counter.next(), Some(5));
+    assert_eq!(counter.next(), Some(6));
+    assert_eq!(counter.next(), None);
+    assert_eq!(counter.next(), None);
+    assert_eq!(counter.next(), None);
+
+    println!("Counter exhausted: {:#?}", counter);
+
+    let sum_util_10: usize = Counter::new(10).sum();
+    assert_eq!(sum_util_10, 55);
+
+    let powers_of_2: Vec<usize> = Counter::new(8).map(|n| 2usize.pow(n as u32)).collect();
+    assert_eq!(powers_of_2, vec![2, 4, 8, 16, 32, 64, 128, 256]);
+
+    println!("--- Unit 7/10 ---");
+    assert_eq!(Container::new(42).value, 42);
+    assert_eq!(Container::new(3.14).value, 3.14);
+    assert_eq!(Container::new("Foo").value, "Foo");
+    assert_eq!(Container::new(String::from("Bar")).value, String::from("Bar"));
+    assert_eq!(Container::new(true).value, true);
+    assert_eq!(Container::new(-12).value, -12);
+    assert_eq!(Container::new(Some("text")).value, Some("text"));
+
+    println!("--- Unit 8/10 ---");
+    let data = vec![4, 1, 1, 2, 1, 3, 3, -2, -2, -2, 5, 5];
+    assert_eq!(
+        Groups::new(data).into_iter().collect::<Vec<Vec<_>>>(), 
+        vec![
+            vec![4],
+            vec![1, 1],
+            vec![2],
+            vec![1],
+            vec![3, 3],
+            vec![-2, -2, -2],
+            vec![5, 5],
+        ]
+    );
+
+    let data2 = vec![1, 2, 2, 1, 1, 2, 2, 3, 4, 4, 3];
+    assert_eq!(
+        Groups::new(data2).into_iter().collect::<Vec<Vec<_>>>(), 
+        vec![
+            vec![1],
+            vec![2, 2],
+            vec![1, 1],
+            vec![2, 2],
+            vec![3],
+            vec![4, 4],
+            vec![3],
+        ]
+    );
 }
