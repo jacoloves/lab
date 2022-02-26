8dad8f490acbfe8dbcd94421795d1696d6f9d066
jacoloves <5511068t@gmail.com>
2022-02-26 13:08:28 +0900

    add cat

--- c_tool/nanchatte_cat.c
+++ c_tool/nanchatte_cat.c
@@ -23,3 +23,25 @@ int main(int argc, char *argv[])
 
 #define BUFFER_SIZE 2048
 
+static void do_cat(const char *path)
+{
+    int fd;
+    unsigned char buf[BUFFER_SIZE];
+    int n;
+
+    fd = open(path, O_RDONLY);
+    if (fd < 0) die(path);
+    for (;;) {
+        n = read(fd, buf, sizeof buf);
+        if (n < 0) die(path);
+        if (n==0) break;
+        if (write(STDOUT_FILENO, buf, n) < 0) die(path);
+    }
+    if (close(fd) < 0) die(path);
+}
+
+static void die(const char *s)
+{
+    perror(s);
+    exit(1);
+}
