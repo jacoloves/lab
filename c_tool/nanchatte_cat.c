34fd4265b532dbffd60cb68847c0cfb245b9f053
jacoloves <5511068t@gmail.com>
2022-03-03 07:45:13 +0900

    standard output & Makefile add

--- c_tool/nanchatte_cat.c
+++ c_tool/nanchatte_cat.c
@@ -4,19 +4,21 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <err.h>
 
 static void do_cat(const char *path);
 static void die(const char *s);
+static void do_line();
 
 int main(int argc, char *argv[])
 {
     int i;
     if (argc < 2) {
-        fprintf(stderr, "%s: file name out given\n", argv[0]);
-        exit(1);
+        do_line();
+        exit(0);
     }
     for (i=1; i<argc; i++) {
-        do_cat(argc[i]);
+        do_cat(argv[i]);
     }
     exit(0);
 }
@@ -44,4 +46,17 @@ static void die(const char *s)
 {
     perror(s);
     exit(1);
+}
+
+static void do_line()
+{
+    unsigned char buf[BUFFER_SIZE];
+    int n; 
+    for (;;) {
+        n = read(STDOUT_FILENO, buf, sizeof buf);
+        if (write(STDOUT_FILENO, buf, n) < 0) {
+            err(EXIT_FAILURE, "write_error");
+            exit(1);
+        } 
+    }
 }
