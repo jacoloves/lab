4ace237930e8ba2a75f8dccb5f06b8a97ca17499
jacoloves <5511068t@gmail.com>
2022-03-10 12:38:43 +0900

    tasks.rs mod

--- rust_project/msRsut_smp/rusty-journal/src/tasks.rs
+++ rust_project/msRsut_smp/rusty-journal/src/tasks.rs
@@ -1,8 +1,14 @@
-use chrono::{DateTime, Utc};
-
-#[derive(Debug)]
+use chrono::{serde::ts_seconds, DateTime, Local, Utc};
+use serde::Deserialize;
+use serde::Serialize;
+use std::io::{Error, ErrorKind, Result, Seek, SeekFrom};
+use std::path::PathBuf;
+use std::fs::{File, OpenOptions};
+#[derive(Debug, Deserialize, Serialize)]
 pub struct Task {
     pub text: String,
+
+    #[serde(with = "ts_seconds")]
     pub created_at: DateTime<Utc>,
 }
 
@@ -12,3 +18,34 @@ impl Task {
         Task { text, created_at }
     }
 }
+
+pub fn add_task(jornal_path: PathBuf, task: Task) -> Result<()> {
+    let mut file = OpenOptions::new().read(true).write(true).create(true).open(jornal_path)?;
+
+    let mut tasks: Vec<Task> = match serde_json::from_reader(&file) {
+        Ok(tasks) => tasks,
+        Err(e) if e.is_eof() => Vec::new(), 
+        Err(e) => Err(e)?,
+    };
+
+    file.seek(SeekFrom::Start(0))?;
+
+    tasks.push(task);
+    serde_json::to_writer(file, &tasks)?;
+
+    Ok(())
+}
+
+pub fn complete_task(journal_path: PathBuf, task_position: usize) -> Result<()> {
+    let file = OpenOptions::new().read(true).write(true).open(jornal_path)?;
+
+    let tasks = match serde_json::from_reader(file) {
+        Ok(tasks) => tasks,
+        Err(e) if e.is_eof() => Vec::new(),
+        Err(e) => Err(e)?,
+    };
+
+    
+}
+
+pub fn liset_tasks(journal_path: PathBuf) -> Result<()> {}
