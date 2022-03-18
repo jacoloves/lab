908b2ac645a26a75c0cd9682af140056241e62b9
jacoloves <5511068t@gmail.com>
2022-03-18 20:36:02 +0900

    祝日の入力を完了

--- go_tool/holiday_api/holiday_2023.go
+++ go_tool/holiday_api/holiday_2023.go
@@ -1,85 +1,89 @@
 package main
 
-type Holiday_2022 struct {
+type Holiday_2023 struct {
 	Year string    `json:"year"`
-	Data Data_2022 `json: "data"`
+	Data Data_2023 `json: "data"`
 }
 
-type Data_2022 []struct {
+type Data_2023 []struct {
 	Title string `json:"Title"`
 	Date  string `json:"Date"`
 }
 
-func holiday_2022() Holiday_2022 {
-	holidays_2022 := Holiday_2022{
-		Year: "2022",
-		Data: Data_2022{
+func holiday_2023() Holiday_2023 {
+	holidays_2023 := Holiday_2023{
+		Year: "2023",
+		Data: Data_2023{
 			{
 				Title: "元旦",
-				Date:  "2022-01-01",
+				Date:  "2023-01-01",
+			},
+			{
+				Title: "休日",
+				Date:  "2023-01-02",
 			},
 			{
 				Title: "成人の日",
-				Date:  "2022-01-10",
+				Date:  "2023-01-09",
 			},
 			{
 				Title: "建国記念の日",
-				Date:  "2022-02-11",
+				Date:  "2023-02-11",
 			},
 			{
 				Title: "天皇誕生日",
-				Date:  "2022-02-23",
+				Date:  "2023-02-23",
 			},
 			{
 				Title: "春分の日",
-				Date:  "2022-03-21",
+				Date:  "2023-03-21",
 			},
 			{
 				Title: "昭和の日",
-				Date:  "2022-04-29",
+				Date:  "2023-04-29",
 			},
 			{
 				Title: "憲法記念日",
-				Date:  "2022-05-03",
+				Date:  "2023-05-03",
 			},
 			{
 				Title: "みどりの日",
-				Date:  "2022-05-04",
+				Date:  "2023-05-04",
 			},
 			{
 				Title: "こどもの日",
-				Date:  "2022-05-05",
+				Date:  "2023-05-05",
 			},
 			{
 				Title: "海の日",
-				Date:  "2022-07-18",
+				Date:  "2023-07-17",
 			},
 			{
 				Title: "山の日",
-				Date:  "2022-08-11",
+				Date:  "2023-08-11",
 			},
 			{
 				Title: "敬老の日",
-				Date:  "2022-09-19",
+				Date:  "2023-09-18",
 			},
 			{
 				Title: "秋分の日",
-				Date:  "2022-09-23",
+				Date:  "2023-09-23",
 			},
 			{
 				Title: "スポーツの日",
-				Date:  "2022-10-10",
+				Date:  "2023-10-09",
 			},
 			{
 				Title: "文化の日",
-				Date:  "2022-11-03",
+				Date:  "2023-11-03",
 			},
 			{
 				Title: "勤労感謝の日",
-				Date:  "2022-11-23",
+				Date:  "2023-11-23",
 			},
 		},
 	}
 
-	return holidays_2022
+	return holidays_2023
 }
