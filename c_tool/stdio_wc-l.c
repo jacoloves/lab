b1dc2182a2bc261e4102ddace621e75e2c02b3ef
jacoloves <5511068t@gmail.com>
2022-04-11 11:13:57 +0900

    stdio_wc-l.c main function add

---
+++ c_tool/stdio_wc-l.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <err.h>
+#include <errno.h>
+#include <string.h>
+
+#define BUFFER_SIZE 2048
+
+int main(int argc, char *argv[])
+{
+    int i;
+    int sum = 0;
+    if (argc < 2) {
+        exit(0);
+    }
+
+    for (i=1; i<argc; ++i) {
+        do_stdio_wcl(argv[i], &sum);
+    }
+    exit(0);
+}
