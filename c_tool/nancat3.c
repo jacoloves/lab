b010b04dd7317cd2fa5e03b5b02cc35935f0e7dd
jacoloves <5511068t@gmail.com>
2022-04-14 10:44:42 +0900

    nancat3.c process complete

--- c_tool/nancat3.c
+++ c_tool/nancat3.c
@@ -7,6 +7,8 @@
 
 #define BUFFER_SIZE 2048
 
+static void nancat(const char *path);
+
 int main(int argc, char *argv[])
 {
     int i;
@@ -16,6 +18,34 @@ int main(int argc, char *argv[])
     }
 
     for (i=1; i<argc; ++i) {
-        
+        nancat(argv[i]);
+    }
+
+    exit(0);
+}
+
+static void nancat(const char *path)
+{
+    FILE *fp;
+    size_t fr;
+    unsigned char buf[BUFFER_SIZE];
+
+    if (NULL == (fp = fopen(path, "r"))) {
+       perror("fopen failed");
+       exit(1);
+    }
+
+    for(;;) {
+        fr = fread(buf, sizeof(char), BUFFER_SIZE, fp);
+        if (fwrite(buf, sizeof(char), fr, stdout) < 0) {
+            perror("fwrite, failed");
+            exit(1);
+        }
+        if (fr < sizeof buf) break;
+    }
+
+    if (fclose(fp)) {
+        perror("fclose error");
+        exit(1);
     }
 }
