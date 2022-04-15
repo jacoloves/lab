a53d2fcf611e555d784538a72b56c69101dfb536
jacoloves <5511068t@gmail.com>
2022-04-15 10:31:47 +0900

    head.c main function modified

--- c_tool/head.c
+++ c_tool/head.c
@@ -5,11 +5,30 @@ static void do_head(FILE *f, long nlines);
 
 int main(int argc, char *argv[])
 {
+    long nlines;
+
     if (argc != 2) {
-        fprintf(stderr, "Usage: %s n\n", argv[0]);
+        fprintf(stderr, "Usage: %s n [file file...]\n", argv[0]);
         exit(1);
     }
-    do_head(stdin, atol(argv[1]));
+    nlines = atol(argv[1]);
+    if (argc == 2) {
+        do_head(stdin, nlines);
+    } else {
+        int i;
+
+        for (i=2; i<argc; i++) {
+            FILE *f;
+
+            f = fopen(argv[i], "r");
+            if (!f) {
+                perror(argv[i]);
+                exit(1);
+            }
+            do_head(f, nlines);
+            fclose(f);
+        }
+    }
     exit(0);
 }
 
