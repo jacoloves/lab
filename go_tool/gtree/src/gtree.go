86a722cd19994a0f2044e2458a04d1dfde8b7ba7
jacoloves <5511068t@gmail.com>
2022-05-29 09:35:37 +0900

    gtree modified

--- go_tool/gtree/src/gtree.go
+++ go_tool/gtree/src/gtree.go
@@ -4,44 +4,90 @@ import (
 	"fmt"
 	"io/fs"
 	"path/filepath"
+	"strings"
 )
 
 const (
-	DirBrunch  string = "|--"
-	LastBrunch string = "`--"
-	DepthSpace string = "|   "
+	DirBrunch   string = "├──"
+	LastBrunch  string = "└──"
+	BrunchSpace string = "│   "
+	DepthSpace  string = "   "
 )
 
-func dirWalker() []string {
+type PathCondition struct {
+	filename   string
+	depth      int
+	isDir      bool
+	isLastFile bool
+}
+
+func getPathCondition(path string, dirFlag bool) PathCondition {
+	var pathcon PathCondition
+	pathcon.depth = strings.Count(path, "/")
+	pathcon.isLastFile = false
+	_, pathcon.filename = filepath.Split(path)
+	if dirFlag {
+		pathcon.isDir = true
+	} else {
+		pathcon.isDir = false
+	}
+	return pathcon
+}
+
+func dirWalker() ([]string, []PathCondition) {
 	var paths []string
+	var pcs []PathCondition
+	cnt := 0 // WalkDir do count
 	err := filepath.WalkDir(".", func(path string, d fs.DirEntry, err error) error {
+		notCurrentDirFlg := false
+		dirFlg := false
 		if err != nil {
 			fmt.Printf("prevent panic by handling failure accessing a path %q: %v\n", path, err)
 			return err
 		}
+
 		if d.IsDir() {
-			// through
+			if cnt == 0 {
+				// through
+			} else {
+				paths = append(paths, filepath.Join(".", path))
+				notCurrentDirFlg = true
+				dirFlg = true
+			}
 		} else {
 			paths = append(paths, filepath.Join(".", path))
+			notCurrentDirFlg = true
 		}
 
+		if notCurrentDirFlg {
+			pcs = append(pcs, getPathCondition(path, dirFlg))
+		}
+		cnt++
 		return nil
 	})
 	if err != nil {
 		fmt.Printf("error walking the path %q: %v\n", ".", err)
 	}
 
-	return paths
+	return paths, pcs
 }
 
-func displayTree(paths []string) {
+func displayTree(pcs []PathCondition) {
 	fmt.Println(".")
-	for _, name := range paths {
-		fmt.Printf("%s%s\n", DirBrunch, name)
+	for _, pathcon := range pcs {
+		if pathcon.depth == 0 {
+			fmt.Printf("%s%s\n", DirBrunch, pathcon.filename)
+		} else {
+			var depthString string
+			for i := 0; i < pathcon.depth; i++ {
+				depthString += BrunchSpace
+			}
+			fmt.Printf("%s%s%s\n", depthString, DirBrunch, pathcon.filename)
+		}
 	}
 }
 
 func main() {
-	currentDirFiles := dirWalker()
-	displayTree(currentDirFiles)
+	_, pcs := dirWalker()
+	displayTree(pcs)
 }
