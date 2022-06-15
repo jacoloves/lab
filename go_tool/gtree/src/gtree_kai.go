8b9c8ed68096ca9e6611b0c7e40e412ffd200b1c
jacoloves <5511068t@gmail.com>
2022-06-15 17:59:19 +0900

    gtree_kai.go add

---
+++ go_tool/gtree/src/gtree_kai.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path"
+	"sort"
+)
+
+const (
+	DirBrunch   string = "├──"
+	LastBrunch  string = "└──"
+	BrunchSpace string = "│   "
+	DepthSpace  string = "    "
+)
+
+type Counter struct {
+	dirs  int
+	files int
+}
+
+func (counter *Counter) index(path string) {
+	stat, _ := os.Stat(path)
+	if stat.IsDir() {
+		counter.dirs += 1
+	} else {
+		counter.files += 1
+	}
+}
+
+func (counter *Counter) output() string {
+	return fmt.Sprintf("\n%d directories, %d files", counter.dirs, counter.files)
+}
+
+func dirnamesFrom(base string) []string {
+	file, err := os.Open(base)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	names, _ := file.Readdirnames(0)
+	file.Close()
+
+	sort.Strings(names)
+	return names
+}
+
+func tree(counter *Counter, base string, prefix string) {
+	names := dirnamesFrom(base)
+
+	for index, name := range names {
+		if name[0] == '.' {
+			continue
+		}
+		subpath := path.Join(base, name)
+		counter.index(subpath)
+
+		if index == len(names)-1 {
+			fmt.Println(prefix+LastBrunch, name)
+			tree(counter, subpath, prefix+DepthSpace)
+		} else {
+			fmt.Println(prefix+DirBrunch, name)
+			tree(counter, subpath, prefix+BrunchSpace)
+		}
+	}
+}
+
+func main() {
+	var directory string
+	if len(os.Args) > 1 {
+		directory = os.Args[1]
+	} else {
+		directory = "."
+	}
+
+	counter := new(Counter)
+	fmt.Println(directory)
+
+	tree(counter, directory, "")
+	fmt.Println(counter.output())
+}
