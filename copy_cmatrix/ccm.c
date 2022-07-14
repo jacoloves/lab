2ab39815edb9976a6c89d649635bcf8a2afcac56
jacoloves <5511068t@gmail.com>
2022-07-14 13:31:24 +0900

    terminal color set

--- copy_cmatrix/ccm.c
+++ copy_cmatrix/ccm.c
@@ -47,5 +47,36 @@ int main(int argc, char *argv[]) {
     savetty();
     nonl();
     cbreak();
-
+    noecho();
+    timeout(0);
+    leaveok(stdscr, TRUE);
+    curs_set(0);
+    
+    // check color
+    if (has_colors()) {
+        start_color();
+#ifdef HAVE_USE_DEFAULT_COLORS
+        if (use_default_color() != ERR) {
+            init_pair(COLOR_BLACK, -1, -1);
+            init_pair(COLOR_GREEN, COLOR_GREEN, -1);
+            init_pair(COLOR_WHITE, COLOR_WHITE, -1);
+            init_pair(COLOR_RED, COLOR_RED, -1);
+            init_pair(COLOR_CYAN, COLOR_CYAN, -1);
+            init_pair(COLOR_MAGENTA, COLOR_MAGENTA, -1);
+            init_pair(COLOR_BLUE, COLOR_BLUE, -1);
+            init_pair(COLOR_YELLOW, COLOR_YELLOW, -1);
+        } else {
+#else
+        {
+#endif
+            init_pair(COLOR_BLACK, COLOR_BLACK, COLOR_BLACK);
+            init_pair(COLOR_GREEN, COLOR_GREEN, COLOR_BLACK);
+            init_pair(COLOR_WHITE, COLOR_WHITE, COLOR_BLACK);
+            init_pair(COLOR_RED, COLOR_RED, COLOR_RED);
+            init_pair(COLOR_CYAN, COLOR_CYAN, COLOR_CYAN);
+            init_pair(COLOR_MAGENTA, COLOR_MAGENTA, COLOR_MAGENTA);
+            init_pair(COLOR_BLUE, COLOR_BLUE, COLOR_BLUE);
+            init_pair(COLOR_YELLOW, COLOR_YELLOW, COLOR_YELLOW);
+        }
+    }
 }
