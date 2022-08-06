d1ab3027c98317e9dda5c82525ffeb2fb04ff335
jacoloves <5511068t@gmail.com>
2022-08-07 02:04:13 +0900

    rust 13th

---
+++ rust_project/book-rust/src/ch3/rand_xorshift.rs
@@ -0,0 +1,23 @@
+use std::time::{SystemTime, UNIX_EPOCH};
+
+fn main() {
+    let mut seed = rand_init();
+
+    for _ in 0..100 {
+        let c = rand(&mut seed, 1, 6);
+        println!("{}", c);
+    }
+}
+
+fn rand_init() -> u32 {
+    SystemTime::now()
+        .duration_since(UNIX_EPOCH).unwrap()
+        .as_millis() as u32
+}
+
+fn rand(seed: &mut u32, start: u32, end: u32) -> u32 {
+    *seed ^= *seed << 13; 
+    *seed ^= *seed >> 17; 
+    *seed ^= *seed << 5; 
+    return *seed % (end - start + 1) + start;
+}
