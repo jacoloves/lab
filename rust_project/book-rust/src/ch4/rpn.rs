f708c6af8bd2ae0d17655e04ad52525ea28e6ae5
jacoloves <5511068t@gmail.com>
2022-08-31 18:13:33 +0900

    rust 16th

--- rust_project/book-rust/src/ch4/rpn.rs
+++ rust_project/book-rust/src/ch4/rpn.rs
@@ -1,13 +1,13 @@
 use std::io;
 fn main() {
     let mut stack: Vec<f64> = vec![];
-    println("RPN:");
+    println!("RPN:");
     let mut s = String::new();
     io::stdin().read_line(&mut s).expect("input error");
     let tokens = s.split_whitespace();
     for tok in tokens {
         let t = tok.trim();
-        match t.parse<f64>() {
+        match t.parse::<f64>() {
             Ok(v) => { stack.push(v); continue; },
             Err(_) => 0.0,
         };
