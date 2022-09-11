a1a503ef2d947dbae30bc04a28d209e3295faf33
jacoloves <5511068t@gmail.com>
2022-09-11 21:44:15 +0900

    add mark

--- rust_project/rust_tool/stopwatch/src/main.rs
+++ rust_project/rust_tool/stopwatch/src/main.rs
@@ -5,19 +5,27 @@ fn main() {
     let mut passed_time_hour: i32 = 0;
     let mut passed_time_min: i32 = 0;
     loop {
-        // hour
-        passed_time_hour += passed_time_sec / 3600;
-        passed_time_sec = passed_time_sec % 3600;
-
-        // minute
-        passed_time_min += passed_time_sec / 60;
-        passed_time_sec = passed_time_sec % 60;
-    
-        // display indication
-        let s = format!("{}:{}:{}\r", passed_time_hour, passed_time_min, passed_time_sec);
-        eprint!("{}", s);
-
+        progress_mark(passed_time_sec);
+        progress_time(&mut passed_time_hour, &mut passed_time_min, &mut passed_time_sec);
         thread::sleep(Duration::new(1,0));
         passed_time_sec += 1;
     }
+}
+
+fn progress_time(hour: &mut i32, min: &mut i32, sec: &mut i32) {
+    // hour
+    *hour += *sec / 3600;
+    *sec = *sec % 3600;
+    
+    // minute
+    *min += *sec / 60;
+    *sec = *sec % 60;
+
+    eprint!("{}:{}:{}\r", hour, min, sec);
+}
+
+fn progress_mark(sec: i32) {
+    let mark = vec!["-", "\\", "|", "/"];
+    let pos = sec as usize % mark.len();
+    eprint!("{} ", mark[pos]);
 }
